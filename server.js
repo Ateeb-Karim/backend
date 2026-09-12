9127ce9c434af384c3c9ecb017861b8a383308a3
Ateeb-Karim <ateebkarim.ak02@gmail.com>
2026-09-12 22:47:30 +0500

    commiting

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express();
 
 app.get("/", (request, response) => {
     response.send("hello world from the server")
+    // this message will be seen on the screen in the website http://localhost:3000
 })
 
 app.listen(3000)
